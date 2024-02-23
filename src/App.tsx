e89a2fa183b8fbad58a522debd275b3a9918e8bc
ArdicMehmet <ardicmehmet96@gmail.com>
2024-02-23 23:31:14 +0300

    Created header component

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { RegisterPage } from './pages/register'
-import { LoginPage } from './pages/login'
+import { LoginPage } from './pages/login/index'
+import Header from './components/header'
+
 
 function App() {
 
   return (
     <>
-      <LoginPage/>
+      <Header/>
     </>
   )
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import logo from '../images/logo.png'
+import heartImage from '../images/Heart.png'
+import profileImage from '../images/Profile.png'
+import shopImage from '../images/Shop.png'
+
+function Header() {
+  return (
+    <header className='w-full h-[120px] flex justify-between ps-[60px] pe-[60px]'>
+        <div className="brand flex justify-center items-center w-1/6">
+            <img className='w-[60px] h-[39px]' src={logo} alt="logo" />
+        </div>
+        <div className='flex justify-center items-center w-4/6'>
+            <div className='findContainer w-[800px] relative text-[20px]'>
+                <input type="text" className='w-full h-[50px] outline-0 ps-[40px] rounded bg-gray-50' placeholder='Search'/>
+            </div>
+            
+        </div>
+        <div className='profileMenu flex justify-center items-center w-1/6'>
+            <img src={profileImage} className='me-2 hover:cursor-pointer' alt="profilePhoto" />
+            <img src={heartImage} className='me-2 hover:cursor-pointer' alt="heartPhoto" />
+            <img src={shopImage} className='hover:cursor-pointer' alt="shopPhoto" />
+        </div>
+    </header>
+  )
+}
+
+export default Header
